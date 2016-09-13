b7560bc8cf0234998e7f25d817c4b637f8150fa0
PortableTomb <tenderyoungpony@gmail.com>
2016-09-13 15:38:29 -0700

    linting, html validation

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-// AJAX / JSON
-var $xhr = $.getJSON('https://soundcloud.com/oembed?url=https%3A%2F%2Fsoundcloud.com%2Fforss%2Fflickermood');
-// var $xhr = $.getJSON('https://soundcloud.com/oembed?url=https%3A%2F%2Fsoundcloud.com%2tycho%2tycho-division');
-
-$xhr.done(function(data) {
-    if ($xhr.status !== 200) {
-        return;
-    }
-    console.log(data);
-});
-
-// https://soundcloud.com/tycho/tycho-division
 
 // ANIMATION
 $(function() {
@@ -19,10 +7,10 @@ $(function() {
 
    var w = window.innerWidth;
    var h = window.innerHeight;
-   canvas.width = w;
+    canvas.width = w;
 	canvas.height = h;
 	ctx.fillStyle = color;
-	ctx.lineWidth = .75;
+	ctx.lineWidth = 0.75;
 	ctx.strokeStyle = color;
 
 	var mousePosition = {
@@ -34,84 +22,80 @@ $(function() {
     num: 33,
     distance: 600,
     d_radius: 750,
-    velocity: -.1,
+    velocity: -0.1,
     array: []
 	};
 
 	function Dot(){
-		this.x = Math.random() * w;
-		this.y = Math.random() * h;
-		this.vx = dots.velocity + Math.random();
-		this.vy = dots.velocity + Math.random();
-		this.radius = Math.random() * 2;
+	  this.x = Math.random() * w;
+	  this.y = Math.random() * h;
+	  this.vx = dots.velocity + Math.random();
+	  this.vy = dots.velocity + Math.random();
+	  this.radius = Math.random() * 2;
 	}
-	Dot.prototype = {
-		create: function(){
-			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.radius, 10, Math.PI * 3, false);
-			ctx.fill();
-		},
-		animate: function() {
-			for(i = 0; i < dots.num; i++){
-				var dot = dots.array[i];
-				if(dot.y < 0 || dot.y > h){
-					dot.vx = dot.vx;
-					dot.vy = - dot.vy;
-				}
-				else if(dot.x < 0 || dot.x > w){
-					dot.vx = - dot.vx;
-					dot.vy = dot.vy;
-				}
-				dot.x += dot.vx;
-				dot.y += dot.vy;
+  Dot.prototype = {
+	create: function(){
+	  ctx.beginPath();
+	  ctx.arc(this.x, this.y, this.radius, 10, Math.PI * 3, false);
+	  ctx.fill();
+	},
+	animate: function() {
+	  for(i = 0; i < dots.num; i++){
+		var dot = dots.array[i];
+		  if(dot.y < 0 || dot.y > h){
+			dot.vx = dot.vx;
+			dot.vy = - dot.vy;
 			}
-		},
-		line: function() {
-			for(i = 0; i < dots.num; i++){
-				for(j = 0; j < dots.num; j++){
-					i_dot = dots.array[i];
-					j_dot = dots.array[j];
+			else if(dot.x < 0 || dot.x > w){
+			  dot.vx = - dot.vx;
+			  dot.vy = dot.vy;
+			}
+			dot.x += dot.vx;
+			dot.y += dot.vy;
+		}
+	},
+line: function() {
+	for(i = 0; i < dots.num; i++){
+	  for(j = 0; j < dots.num; j++){
+		  i_dot = dots.array[i];
+		  j_dot = dots.array[j];
 
-					if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
-						if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
-							ctx.beginPath();
-							ctx.moveTo(i_dot.x, i_dot.y);
-                   ctx.bezierCurveTo(i_dot.x, (h / 2), (w / 2), i_dot.y, j_dot.x, j_dot.y);
-							ctx.stroke();
-							ctx.closePath();
-						}
+		  if((i_dot.x - j_dot.x) < dots.distance && (i_dot.y - j_dot.y) < dots.distance && (i_dot.x - j_dot.x) > - dots.distance && (i_dot.y - j_dot.y) > - dots.distance){
+				if((i_dot.x - mousePosition.x) < dots.d_radius && (i_dot.y - mousePosition.y) < dots.d_radius && (i_dot.x - mousePosition.x) > - dots.d_radius && (i_dot.y - mousePosition.y) > - dots.d_radius){
+					ctx.beginPath();
+					ctx.moveTo(i_dot.x, i_dot.y);
+                    ctx.bezierCurveTo(i_dot.x, (h / 2), (w / 2), i_dot.y, j_dot.x, j_dot.y);
+					ctx.stroke();
+					ctx.closePath();
 					}
 				}
 			}
 		}
-	};
-
-	function createDots(){
-		ctx.clearRect(0, 0, w, h);
-		for(i = 0; i < dots.num; i++){
-			dots.array.push(new Dot());
-			dot = dots.array[i];
-
-			dot.create();
-		}
-
-		dot.line();
-		dot.animate();
 	}
+};
 
-	$('canvas').on('mousemove mouseleave', function(e){
-		if(e.type == 'mousemove'){
-			mousePosition.x = e.pageX;
-			mousePosition.y = e.pageY;
-		}
-		if(e.type == 'mouseleave'){
-			mousePosition.x = w / 2;
-			mousePosition.y = h / 2;
-		}
-	});
-
+function createDots(){
+  ctx.clearRect(0, 0, w, h);
+	for(i = 0; i < dots.num; i++){
+	  dots.array.push(new Dot());
+	  dot = dots.array[i];
+	  dot.create();
+	}
+	  dot.line();
+	  dot.animate();
+}
+  $('canvas').on('mousemove mouseleave', function(e){
+	if(e.type == 'mousemove'){
+		mousePosition.x = e.pageX;
+		mousePosition.y = e.pageY;
+	}
+	if(e.type == 'mouseleave'){
+		mousePosition.x = w / 2;
+		mousePosition.y = h / 2;
+	}
+});
   $('canvas').on('click', function(){
-		dots.num += 5;
+	dots.num += 5;
 	});
 
 	setInterval(createDots, 10/10);
@@ -120,32 +104,12 @@ $(function() {
     canvas.width = w;
     canvas.height = h;
     ctx.fillStyle = color;
-    ctx.lineWidth = .1;
+    ctx.lineWidth = 0.1;
     ctx.strokeStyle = color;
   });
 });
 
-
 // SITE CONTENT
 $(document).ready(function() {
-
 $(".button-collapse").sideNav();
-// $("#place-order1").click(function() {
-// $('#content-target').children().empty();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
--- js/email-list.js
+++ js/email-list.js
@@ -10,18 +10,14 @@ var $email = $('#email');
 var $myform = $('#myForm');
   $('#submitButton').on('click', function(event) {
 
-      if ($email.val().trim() === '' || validateEmail($email.val()) === false) {
-          Materialize.toast('Please add a valid email address.', 2000);
-          event.preventDefault();
-          return;
+    if ($email.val().trim() === '' || validateEmail($email.val()) === false) {
+      Materialize.toast('Please add a valid email address.', 2000);
+      event.preventDefault();
+      return;
       }
-
-      if (validateEmail($email.val()) === true) {
-          Materialize.toast('You\'ve been added to the list!', 2000);
-           event.preventDefault();
-          return;
+    if (validateEmail($email.val()) === true) {
+      Materialize.toast('You\'ve been added to the list!', 2000);
+      event.preventDefault();
+      return;
       }
-
-
-
   });
